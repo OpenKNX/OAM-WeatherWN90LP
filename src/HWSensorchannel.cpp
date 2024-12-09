90724cc28e9c9eb2049acca3a791785ec8a5bd3c
Ing-Dom <dom@ing-dom.de>
2024-12-09 09:04:36 +0100

    complete reading of all values into HWSensors

--- src/HWSensorchannel.cpp
+++ src/HWSensorchannel.cpp
@@ -16,6 +16,12 @@ HWSensorchannel::HWSensorchannel()
     mutex_init(&mx1);
     mutex_init(&mx2);
     mutex_init(&mx3);
+    mutex_init(&mx4);
+    mutex_init(&mx5);
+    mutex_init(&mx6);
+    mutex_init(&mx7);
+    mutex_init(&mx8);
+    mutex_init(&mx9);
 }
 
 void HWSensorchannel::Setup(uint8_t adr, uint8_t channel_number)
@@ -51,6 +57,49 @@ bool HWSensorchannel::Loop()
         m_pressure_last_poll_millis = millis();
         return true;
     }
+
+    if(delayCheckMillis(m_light_last_poll_millis, 20000))
+    {
+        SetLight(ReadLight());
+        m_light_last_poll_millis = millis();
+        return true;
+    }
+
+    if(delayCheckMillis(m_uvi_last_poll_millis, 20000))
+    {
+        SetUVI(ReadUVI());
+        m_uvi_last_poll_millis = millis();
+        return true;
+    }
+
+    if(delayCheckMillis(m_wind_last_poll_millis, 20000))
+    {
+        SetWind(ReadWind());
+        m_wind_last_poll_millis = millis();
+        return true;
+    }
+
+    if(delayCheckMillis(m_gust_last_poll_millis, 20000))
+    {
+        SetGust(ReadGust());
+        m_gust_last_poll_millis = millis();
+        return true;
+    }
+
+    if(delayCheckMillis(m_winddir_last_poll_millis, 20000))
+    {
+        SetWindDir(ReadWindDir());
+        m_winddir_last_poll_millis = millis();
+        return true;
+    }
+
+    if(delayCheckMillis(m_rain_last_poll_millis, 20000))
+    {
+        SetRain(ReadRain());
+        m_rain_last_poll_millis = millis();
+        return true;
+    }
+    
     return true;
 }
 
@@ -345,6 +394,98 @@ float HWSensorchannel::GetPressure()
     }
 }
 
+float HWSensorchannel::GetLight()
+{
+    float retval;
+    if(mutex_try_enter(&mx4, nullptr))
+    {
+        retval = m_light;
+        mutex_exit(&mx4);
+        return retval;
+    }
+    else
+    {
+        return NAN;
+    }
+}
+
+uint8_t HWSensorchannel::GetUVI()
+{
+    uint8_t retval;
+    if(mutex_try_enter(&mx5, nullptr))
+    {
+        retval = m_uvi;
+        mutex_exit(&mx5);
+        return retval;
+    }
+    else
+    {
+        return NAN;
+    }
+}
+
+float HWSensorchannel::GetWind()
+{
+    float retval;
+    if(mutex_try_enter(&mx6, nullptr))
+    {
+        retval = m_wind;
+        mutex_exit(&mx6);
+        return retval;
+    }
+    else
+    {
+        return NAN;
+    }
+}
+
+float HWSensorchannel::GetGust()
+{
+    float retval;
+    if(mutex_try_enter(&mx7, nullptr))
+    {
+        retval = m_gust;
+        mutex_exit(&mx7);
+        return retval;
+    }
+    else
+    {
+        return NAN;
+    }
+}
+
+uint8_t HWSensorchannel::GetWindDir()
+{
+    uint8_t retval;
+    if(mutex_try_enter(&mx8, nullptr))
+    {
+        retval = m_winddir;
+        mutex_exit(&mx8);
+        return retval;
+    }
+    else
+    {
+        return NAN;
+    }
+}
+
+float HWSensorchannel::GetRain()
+{
+    float retval;
+    if(mutex_try_enter(&mx9, nullptr))
+    {
+        retval = m_rain;
+        mutex_exit(&mx9);
+        return retval;
+    }
+    else
+    {
+        return NAN;
+    }
+}
+
+
+
 void HWSensorchannel::SetTemperature(float temperature)
 {
     logDebugP("Temp value %f", temperature);
@@ -365,6 +506,48 @@ void HWSensorchannel::SetPressure(float pressure)
     mutex_enter_blocking(&mx3);
     m_pressure = pressure;
     mutex_exit(&mx3);
+}
+
+void HWSensorchannel::SetLight(float light)
+{
+    mutex_enter_blocking(&mx4);
+    m_light = light;
+    mutex_exit(&mx4);
+}
+
+void HWSensorchannel::SetUVI(uint8_t uvi)
+{
+    mutex_enter_blocking(&mx5);
+    m_uvi = uvi;
+    mutex_exit(&mx5);
+}
+
+void HWSensorchannel::SetWind(float wind)
+{
+    mutex_enter_blocking(&mx6);
+    m_wind = wind;
+    mutex_exit(&mx6);
+}   
+
+void HWSensorchannel::SetGust(float gust)
+{
+    mutex_enter_blocking(&mx7);
+    m_gust = gust;
+    mutex_exit(&mx7);
+}
+
+void HWSensorchannel::SetWindDir(uint8_t winddir)
+{
+    mutex_enter_blocking(&mx8);
+    m_winddir = winddir;
+    mutex_exit(&mx8);
+}
+
+void HWSensorchannel::SetRain(float rain)
+{
+    mutex_enter_blocking(&mx9);
+    m_rain = rain;
+    mutex_exit(&mx9);
 }
 
 const std::string HWSensorchannel::name()
